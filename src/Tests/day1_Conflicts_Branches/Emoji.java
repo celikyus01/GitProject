6866ae3399b930801b5b0b0ce17e5b678925f377
celikyus01 <celikyus01@gmail.com>
2020-05-13 17:06:50 +0100

    add Emoji.java

---
+++ src/Tests/day1_Conflicts_Branches/Emoji.java
@@ -0,0 +1,16 @@
+package Tests.day1_Conflicts_Branches;
+
+public class Emoji {
+
+    public static void main(String[] args) {
+
+        System.out.println("User can send emoji");
+
+        //git branch shows howmony branh we have
+        //git branch branchname  --> create an new branch
+        //git checkout branchname --> switch to related branch
+    }
+
+
+
+}

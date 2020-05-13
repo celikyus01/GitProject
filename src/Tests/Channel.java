b53dd191c71b213febd1ba10be37ea0dd67a3949
celikyus01 <celikyus01@gmail.com>
2020-05-13 15:56:07 +0100

    add Channel.java

---
+++ src/Tests/Channel.java
@@ -0,0 +1,19 @@
+package Tests;
+
+public class Channel     {
+
+    public static void main(String[] args) {
+
+        int number=5;
+
+        if(number==5){
+            System.out.println("correct");
+        }
+
+
+    }
+
+
+
+
+}

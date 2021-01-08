af0b1b83bb9f7edf2a435cc3573429573ea62f44
celikyus01 <62037357+celikyus01@users.noreply.github.com>
2021-01-08 20:23:53 +0000

    Update deneme1.java

--- src/Tests/day1_Conflicts_Branches/deneme1.java
+++ src/Tests/day1_Conflicts_Branches/deneme1.java
@@ -3,5 +3,10 @@ package Tests.day1_Conflicts_Branches;
 public class deneme1 {
 
     //comment1
-    //comment2
+    
+    //github desisiklik
+    //comment2 -56
+    // github degisiklik
+    
+    
 }

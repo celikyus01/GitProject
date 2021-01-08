56bfb3d80b9ed344d4368348964121d1da1c2274
celikyus01 <62037357+celikyus01@users.noreply.github.com>
2021-01-08 21:29:09 +0000

    Update Call.java

--- src/Tests/day1_Conflicts_Branches/Call.java
+++ src/Tests/day1_Conflicts_Branches/Call.java
@@ -3,7 +3,7 @@ package Tests.day1_Conflicts_Branches;
 public class Call {
 
     public static void main(String[] args) {
-        //call class is //change7 created
+        //call 5
         //main class created
 
         //change1

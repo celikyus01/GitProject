1f54bcc37f675806594dc325b553866c802428d1
celikyus01 <celikyus01@gmail.com>
2021-01-08 21:09:08 +0000

    8

--- src/Tests/day1_Conflicts_Branches/Call.java
+++ src/Tests/day1_Conflicts_Branches/Call.java
@@ -4,7 +4,8 @@ public class Call {
 
     public static void main(String[] args) {
         //call class is created
-        //main class created
+        //change8
+        // created
 
         //change1
 
